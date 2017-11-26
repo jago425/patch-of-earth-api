1d1ed247cf0475a170c21b27cd8b6f926a073108
jago425 <rmjagodowski@gmail.com>
2017-11-26 16:51:19 -0500

    Create plants and index plants work!
    -created curl script for create
    -tested index in the browser

---
+++ scripts/plants/create-plant.sh
@@ -0,0 +1,25 @@
+# TEST
+curl --include --request POST "http://localhost:4741/plants" \
+--header "Content_Type: application/json" \
+--data '{
+  "plant": {
+    "plant_type": "'"${TYPE}"'",
+    "common_name": "'"${COMMON}"'",
+    "scientific_name": "'"${SCI}"'",
+    "variety": "'"${VAR}"'",
+    "brand": "'"${BRAND}"'"
+  }
+}'
+
+# PRODUCTION
+# curl --include --request POST "https://evening-mountain-89646.herokuapp.com/plants" \
+# --header "Content_Type: application/json" \
+# --data '{
+#   "plant": {
+#     "plant_type": "'"${TYPE}"'",
+#     "common_name": "'"${COMMON}"'",
+#     "scientific_name": "'"${SCI}"'",
+#     "variety": "'"${VAR}"'",
+#     "brand": "'"${BRAND}"'"
+#   }
+# }'
