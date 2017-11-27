d178512c953d57be6935408096c00841088d0988
jago425 <rmjagodowski@gmail.com>
2017-11-27 15:53:49 -0500

    Added users to plants
    -updated plants controller to inherit from ProtectedController
    -updated the create and set methods in the controller
    -updated the plants model to belong to users
    -updated the users model to have many plants
    -generated a migration to update the schema

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,4 +1,5 @@
 class Plant < ApplicationRecord
+  belongs_to :user
   has_many :garden_plants
   has_many :gardens, through: :garden_plants
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171127191409) do
+ActiveRecord::Schema.define(version: 20171127204532) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,8 @@ ActiveRecord::Schema.define(version: 20171127191409) do
     t.string "brand"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_plants_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -63,4 +65,5 @@ ActiveRecord::Schema.define(version: 20171127191409) do
   add_foreign_key "examples", "users"
   add_foreign_key "garden_plants", "gardens"
   add_foreign_key "garden_plants", "plants"
+  add_foreign_key "plants", "users"
 end
