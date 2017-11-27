adf74917d30f00b2a1e24e3cc8cdee64de44c555
jago425 <rmjagodowski@gmail.com>
2017-11-27 17:30:07 -0500

    made changes to be able to merge with master

---
+++ scripts/users-plants/destroy-plant.sh
@@ -0,0 +1,9 @@
+curl --include --request DELETE "http://localhost:4741/plants/${ID}" \
+--header "Authorization: Token token=${TOKEN}" \
+--header "Content_Type: application/json"
+
+# PRODUCTION
+# curl --include --request DELETE "https://evening-mountain-89646.herokuapp.com/plants/${ID}" \
+# --header "Authorization: Token token=${TOKEN}" \
+# --header "Content_Type: application/json"
+echo
